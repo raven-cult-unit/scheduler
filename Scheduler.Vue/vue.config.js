ac1ec0e86a8a829c20b2315a564d4b794d16aeaa
Valentin Turcu <6671776+v-echo@users.noreply.github.com>
2019-01-10 12:16:31 +0100

    Initial project templates. Included Docker support.

---
+++ Scheduler.Vue/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  outputDir: undefined,
+  assetsDir: undefined,
+  publicPath: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: false,
+  parallel: undefined,
+  css: undefined
+}
